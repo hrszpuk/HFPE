16d0aa460b3f0f2143449f53dacfb2de0a6f7ea9
tokorv <tokorv@pop-os.localdomain>
2021-06-10 15:06:32 +0100

    Started on beta branch

---
+++ src/Scripts/global.gd
@@ -0,0 +1,10 @@
+extends Node
+# TODO
+# 1. Set up menu scene
+#	- Create button sprites
+#	- Create profile sprites
+#	- Create logo and store assets
+# 2. Make menu scene customisable through config
+
+func _ready():
+	pass
