e773b3c9f617e24495d7b2197cced4ce80aaeef3
tokorv <tokorv.studios@gmail.com>
2021-06-26 00:25:37 +0100

    Configured settings

--- autoload.gd
+++ autoload.gd
@@ -1,3 +1,25 @@
 extends Node
 
 # Autoloads the library, general settings, and the palette.cfg and config.cfg
+
+func _ready():
+	load_palette_config()
+	load_config_config()
+	load_settings_config()
+	load_library_config()
+	
+
+func load_palette_config():
+	pass
+	
+
+func load_config_config():
+	pass
+	
+	
+func load_settings_config():
+	pass
+	
+	
+func load_library_config():
+	pass
