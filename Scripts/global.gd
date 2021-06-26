a596f5524b001cbd312657a7f4a25669b660e601
tokorv <tokorv.studios@gmail.com>
2021-06-26 23:59:24 +0100

    autoload.gd

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,4 +1,10 @@
 extends Node
 
+# config.cfg variables
+var author_username = "Author Username"
+var author_icon_name = "author_icon.png"
+var author_description = "Author Description"
+
 func _ready():
 	pass
+
--- autoload.gd
+++ autoload.gd
@@ -2,24 +2,62 @@ extends Node
 
 # Autoloads the library, general settings, and the palette.cfg and config.cfg
 
-func _ready():
-	load_palette_config()
-	load_config_config()
-	load_settings_config()
-	load_library_config()
-	
+func hf_config():
+	# Load hyperfight config
+	pass
 
-func load_palette_config():
-	pass
 	
+func hf_palette():
+	# Load hyperfight palette
+	pass
 
-func load_config_config():
+	
+func mods():
+	# Scan mod directory
+	# Scan sub directories
+	# Load palette and config files
+	pass 
+
+	
+func library():
+	# Scan directory
+	# Load all *.cfg files
 	pass
-	
-	
-func load_settings_config():
-	pass
-	
-	
-func load_library_config():
-	pass
+
+
+func config():
+	var file: File = File.new()
+	if file.file_exists("user://HFPE/config.cfg"):
+		var config: ConfigFile = ConfigFile.new()
+		config.load("HFPE/config.cfg")
+		config.get_value("Author Settings", "author_username", global.author_username)
+		config.get_value("Author Settings", "author_description", global.author_description)
+		config.get_value("Author Settings", "author_icon", global.author_icon)
+	else:
+		# Generate config
+		var config: ConfigFile = ConfigFile.new()
+		config.set_value("Author Settings", "author_username", global.author_username)
+		config.set_value("Author Settings", "author_description", global.author_description)
+		config.set_value("Author Settings", "author_icon", global.author_icon)
+		config.save("HFPE/config.cfg")
+
+
+func data_config():
+	var file: File = File.new()
+	if file.file_exists("user://HFPE/data/config.cfg"):
+		var config: ConfigFile = ConfigFile.new()
+		config.load("HFPE/data/config.cfg")
+	else:
+		# Generate config
+		var config: ConfigFile = ConfigFile.new()
+		config.save("HFPE/data/config.cfg")
+
+
+func directory():
+	var dir: Directory = Directory.new()
+	dir.open("user://")
+	dir.make_dir("HFPE")
+	dir.make_dir("HFPE/library")
+	dir.make_dir("HFPE/data")
+	dir.make_dir("HFPE/data/assets")
+	dir.make_dir("mods")
