4f9ea6496340ecc6c7ba8b2afc6e1a2e2c6c865b
tokorv <tokorv.studios@gmail.com>
2021-07-03 23:43:55 +0100

    Palette Editor

--- Scripts/Background.gd
+++ Scripts/Background.gd
@@ -18,3 +18,27 @@ func reset_character() -> void:
 func set_character(character: String) -> void:
 	$Character.animation = character
 	return
+	
+
+func set_stage(stage: String) -> void:
+	$Stage.animation = stage
+	return
+	
+
+func reset_character_shader_param(index: int, color: Color) -> void:
+	$Character.material.set_shader_param("threshold", 0.001)
+	$Character.material.set_shader_param("color_o%d" % index, color)
+	return
+	
+
+func set_character_shader_param(index: int, color: Color) -> void:
+	$Character.material.set_shader_param("color_n%d" % index, color)
+	return
+	
+
+func clear_character_shader() -> void:
+	$Character.material.set_shader_param("threshold", 1)
+	for i in range(0, 19):
+		$Character.material.set_shader_param("color_o%d" % i, Color.black)
+	return
+		
--- Scripts/PaletteEditor.gd
+++ Scripts/PaletteEditor.gd
@@ -3,9 +3,38 @@ extends Control
 onready var ColorPickerBackground = $ColorPickerBackground
 onready var ColorPicker = $ColorPicker
 onready var CharacterSelect = $CharacterSelect
+onready var ColorList = $ColorList
+
+var current_color_index = null
 
 func _ready() -> void:
-	$Background.set_character("goto")
+	# check if new if so:
+	# set character to goto
+	# set item options to default goto
+	# if not:
+	# set character to character at current index
+	# set item options to palette at current index
+	# render shaders
+	# All:
+	# setup color picker
+	var i: int = 0
+	if global.state == global.PALETTE_EDITOR_NEW:
+		$Background.set_character("goto")
+		for item in global.default_palettes.goto:
+			ColorList.add_item(item, null, true)
+			$Background.reset_character_shader_param(i, Color(item))
+			$Background.set_character_shader_param(i, Color(item))
+			i += 1
+	elif global.state == global.PALETTE_EDITOR_EDIT:
+		$background.set_character(global.palette_data[global.current_index]["name"])
+		for item in global.palette_data[global.current_index]["palette"]:
+			ColorList.add_item(item, null, true)
+			$Background.reset_character_shader_param(i, Color(global.default_palettes.goto[i]))
+			$Background.set_character_shader_param(i, Color(item))
+			i += 1
+		# Render shaders
+		
+		
 	setup_color_picker()
 	return
 	
@@ -18,5 +47,43 @@ func setup_color_picker() -> void:
 	var position = Vector2(ColorPickerBackground.rect_position.x+10, ColorPickerBackground.rect_position.y+10)
 	ColorPicker.rect_position = position
 	return
+	
+
+func _on_CancelButton_pressed():
+	get_tree().change_scene("res://Scenes/PaletteManager.tscn")
+	
+
+func _on_ColorPicker_color_changed(color):
+	if current_color_index != null:
+		ColorList.set_item_text(current_color_index, Color(color).to_html(false))
+		$Background.set_character_shader_param(current_color_index, Color(color))
+	
+
+func _on_StageSelect_item_selected(index):
+	pass
+
+
+func _on_ColorList_item_selected(index):
+	current_color_index = index
+	ColorPicker.color = Color(ColorList.get_item_text(index))
+
+
+func _on_RandomButton_pressed():
+	if current_color_index != null:
+		var rng = RandomNumberGenerator.new()
+		rng.randomize()
+		var color = Color(rng.randf_range(0.0, 1.0), rng.randf_range(0.0, 1.0), rng.randf_range(0.0, 1.0))
+		ColorList.set_item_text(current_color_index, color.to_html(false))
+		ColorPicker.color = color
+		$Background.set_character_shader_param(current_color_index, Color(color))
+	
+
+func _on_InvertButton_pressed():
+	if current_color_index != null:
+		var color = Color(ColorList.get_item_text(current_color_index)).inverted()
+		ColorPicker.color = color
+		$Background.set_character_shader_param(current_color_index, Color(color))
+		ColorList.set_item_text(current_color_index, color.to_html(false))
+	
 	
 	
